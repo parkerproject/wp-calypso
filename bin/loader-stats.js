d02c7646a1db918eed3c102f07bc669fb5306c6a
Ben Lowery <ben.lowery@automattic.com>
2018-03-15 16:19:09 -0400

    add sizes and siblings to loader stats

--- bin/loader-stats.js
+++ bin/loader-stats.js
@@ -4,20 +4,35 @@ const stats = require( path.join( __dirname, '..', 'server', 'bundler', 'assets.
 
 const whichSection = process.argv[2];
 
+function getChunk( name ) {
+	return stats.chunks.find( chunk => chunk.id === name );
+}
 
-const sectionChunk = stats.chunks.find( chunk => chunk.id === whichSection );
+const sectionChunk = getChunk( whichSection );
 if ( ! sectionChunk ) {
 	console.log( `no section chunk found for ${whichSection}` );
 }
 
-console.log( `stats for ${whichSection}` );
 
-console.log( `  size: \n    ${ sectionChunk.size }` );
-console.log( "  files: " );
-sectionChunk.files.forEach( file => {
-	console.log( `    ${file}`)
-} );
-console.log( "  siblings: " );
-sectionChunk.siblings.forEach( sibling => {
-	console.log( `    ${sibling}`)
-} );
+function getChunkAndSiblings( which ) {
+	const mainChunk = getChunk( which );
+	return [
+		mainChunk,
+		...mainChunk.siblings.map( getChunk )
+	];
+}
+
+
+const chunksToLoad = [
+	...getChunkAndSiblings( 'build' ),
+	...getChunkAndSiblings( whichSection )
+];
+
+console.log( `to load ${whichSection}:` );
+
+chunksToLoad.forEach( chunk => {
+	console.log( `  ${chunk.id} (${chunk.size / 1000 }kb)`);
+})
+
+console.log( "Total: " + ( chunksToLoad.reduce( ( totalSize, chunk ) => totalSize + chunk.size, 0 ) / 1000 ) + "kb" );
+
