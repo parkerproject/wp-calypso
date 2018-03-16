7da3fc2ba6b0f50314b9849dbb09c3f9b753e927
Ben Lowery <ben.lowery@automattic.com>
2018-03-16 13:48:09 -0400

    update the loader stats a bit

--- bin/loader-stats.js
+++ bin/loader-stats.js
@@ -1,38 +1,60 @@
 #!/usr/bin/env node
 const path = require( 'path' );
 const stats = require( path.join( __dirname, '..', 'server', 'bundler', 'assets.json' ) );
+const _ = require( 'lodash' );
+const gzipSize = require( 'gzip-size' );
 
-const whichSection = process.argv[2];
 
-function getChunk( name ) {
-	return stats.chunks.find( chunk => chunk.id === name );
+
+function getChunkByName( name ) {
+	return stats.chunks.find( chunk => chunk.names.indexOf( name ) !== -1 );
 }
 
-const sectionChunk = getChunk( whichSection );
-if ( ! sectionChunk ) {
-	console.log( `no section chunk found for ${whichSection}` );
+function getChunkById( id ) {
+	return stats.chunks.find( chunk => chunk.id === id );
 }
-
 
 function getChunkAndSiblings( which ) {
-	const mainChunk = getChunk( which );
+	const mainChunk = getChunkByName( which );
 	return [
 		mainChunk,
-		...mainChunk.siblings.map( getChunk )
+		...mainChunk.siblings.map( getChunkById )
 	];
 }
 
+const whichSection = process.argv[2];
+const sectionChunk = getChunkByName( whichSection );
+
+if ( ! sectionChunk ) {
+	console.log( `no section chunk found for ${whichSection}` );
+}
 
 const chunksToLoad = [
 	...getChunkAndSiblings( 'build' ),
 	...getChunkAndSiblings( whichSection )
 ];
 
-console.log( `to load ${whichSection}:` );
+const filesToLoad = _.flatMap( chunksToLoad, chunk => {
+	return chunk.files.map( file => path.join( __dirname, '..', 'public', file.replace( '/calypso/', '' ) ) );
+} );
 
-chunksToLoad.forEach( chunk => {
-	console.log( `  ${chunk.id} (${chunk.size / 1000 }kb)`);
-})
+async function calculateSizes() {
+	const fileSizePromises = filesToLoad.map( f => gzipSize.file( f ) );
 
-console.log( "Total: " + ( chunksToLoad.reduce( ( totalSize, chunk ) => totalSize + chunk.size, 0 ) / 1000 ) + "kb" );
+	const fileSizes = await Promise.all( fileSizePromises );
+
+	const filesWithSizes = _.zipObject( filesToLoad, fileSizes );
+
+	console.log( `to load ${whichSection}:` );
+
+	filesToLoad.forEach( f => {
+		console.log( `   ${f}: (${ filesWithSizes[ f ] / 1000 }kb)`);
+	})
+
+	console.log( "Total: " + ( filesToLoad.reduce( ( totalSize, f ) => totalSize + filesWithSizes[ f ], 0 ) / 1000 ) + "kb" );
+}
+
+calculateSizes();
+
+
 
